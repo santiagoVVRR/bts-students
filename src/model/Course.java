1b41c1cf424066501b5bcbc441435ada99479e8f
santiagoVVRR <santiagovalencia27@gmail.com>
2019-05-13 12:33:43 -0500

    inorder method in Course class

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
 
 	private Student root;
@@ -36,10 +39,55 @@ public class Course {
 		
 	}
 	
-	
+	/*
 	public Student searchStudent(int c) {
 
 		
+	}*/
+	
+	public List<Student> preorder(){
+		
+		return preorder(root);
+		
 	}
+	
+	private List<Student> preorder(Student currentNode){
+		List<Student> l = new ArrayList<>();
+		if(currentNode != null) {
+			l.add(currentNode);
+			List<Student> ll = preorder(currentNode.getLeft());
+			List<Student> lr = preorder(currentNode.getRight());
+			l.addAll(ll);
+			l.addAll(lr);
+		}
+		return l;
+	}
+	
+	public List<Student> inorder(){
+		
+		return inorder(root);
+		
+	}
+	
+	private List<Student> inorder(){
+		List<Student> m = ArrayList<>();
+	}
+	/**
+	 * el mismo que el preorder de arriba pero con acumulacion de parametros
+	 * @return
+	 
+	public List<Student> preorder(){
+		List<Student> l = new ArrayList<>()
+		preorder(root,l);
+		return l;
+	}
+	
+	private void preorder(Student cs, List<Student>) {
+		if(cs != null) {
+			l.add(cs);
+			preorder(cs.getLeft(),l);
+			preorder(cs.getRight(),l);
+		}
+	}*/
 
 }
