ac5a0b4fe9cbc3bf8aeba9dca213cfadcd07c19f
santiagoVVRR <santiagovalencia27@gmail.com>
2019-05-13 11:24:04 -0500

    Created the Student class like a bst node

---
+++ src/model/Student.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Student {
+
+	private int code;
+	private String name;
+	
+	private Student right;
+	private Student left;
+	
+	
+	public Student(int code,String name) {
+		this.code = code;
+		this.name = name;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
